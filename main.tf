7578913ea9be838e6fadbaa2a0461948928fd707
Major Hayden <major@mhtx.net>
2023-11-30 14:17:22 -0600

    Use JSON for assume role policy

--- main.tf
+++ main.tf
@@ -37,28 +37,26 @@ resource "aws_iam_openid_connect_provider" "github" {
 }
 
 resource "aws_iam_role" "infrastructure" {
-  assume_role_policy = <<POLICY
-{
-  "Statement": [
-    {
-      "Action": "sts:AssumeRoleWithWebIdentity",
-      "Condition": {
-        "StringEquals": {
-          "token.actions.githubusercontent.com:aud": "sts.amazonaws.com"
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Federated" : "arn:aws:iam::911986281031:oidc-provider/token.actions.githubusercontent.com"
         },
-        "StringLike": {
-          "token.actions.githubusercontent.com:sub": "repo:major/infrastructure:*"
+        "Action" : "sts:AssumeRoleWithWebIdentity",
+        "Condition" : {
+          "StringEquals" : {
+            "token.actions.githubusercontent.com:aud" : "sts.amazonaws.com"
+          },
+          "StringLike" : {
+            "token.actions.githubusercontent.com:sub" : "repo:major/infrastructure:*"
+          }
         }
-      },
-      "Effect": "Allow",
-      "Principal": {
-        "Federated": "arn:aws:iam::911986281031:oidc-provider/token.actions.githubusercontent.com"
       }
-    }
-  ],
-  "Version": "2012-10-17"
-}
-POLICY
+    ]
+  })
 
   description          = "GitHub Actions Terraform OIDC role"
   managed_policy_arns  = ["arn:aws:iam::aws:policy/AdministratorAccess"]
